cddc054fb9145397eeb193947c3b0f1900c18b18
pjwang <pjwang0710@gmail.com>
2021-03-01 13:55:17 +0800

    [Fix] remove special case

--- Week-2/Assignment-5/main.py
+++ Week-2/Assignment-5/main.py
@@ -1,6 +1,5 @@
+
 def binary_search_last(numbers, target):
-    if len(numbers) == 0:
-        return -1
     start_position = 0
     end_position = len(numbers) - 1
     while start_position <= end_position:
@@ -12,8 +11,6 @@ def binary_search_last(numbers, target):
     return start_position
 
 def binary_search_first(numbers, target):
-    if len(numbers) == 0:
-        return -1
     start_position = 0
     end_position = len(numbers) - 1
     while start_position <= end_position:
@@ -29,6 +26,7 @@ def binary_search(method):
         ([1, 2, 5, 5, 5, 6, 7], 2),
         ([1, 2, 5, 5, 5, 6, 7], 5),
         ([1, 2, 5, 5, 5, 6, 7], 3),
+        ([], 1),
         ([1, 2, 5, 5, 5, 6, 7], 0),
         ([1, 2, 5, 5, 5, 6, 7], 1.5),
         ([1, 2, 5, 5, 5, 6, 7], 3),
@@ -37,11 +35,11 @@ def binary_search(method):
         ([1, 2, 5, 5, 5, 6, 7], 6.5),
         ([1, 2, 5, 5, 5, 6, 7], 8)
     ]
-    for test_item in test_array:
+    for test_list, test_target in test_array:
         if method == 'first':
-            print(binary_search_first(test_item[0], test_item[1]))
+            print(binary_search_first(test_list, test_target))
         elif method == 'last':
-            print(binary_search_last(test_item[0], test_item[1]))
+            print(binary_search_last(test_list, test_target))
 
 if __name__ == '__main__':
     binary_search('first')
